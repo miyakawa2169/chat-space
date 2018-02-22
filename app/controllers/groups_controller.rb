8cb0399e6f76632290b3962e76b2e1b04e8ca22b
Jun Miyakawa <fcfym630@gmail.com>
2018-02-22 15:08:54 +0900

    グループ編集機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,15 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to root_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   def new
@@ -20,5 +29,9 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
