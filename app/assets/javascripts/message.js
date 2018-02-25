0bd8584faf19962c4afeba9d84cbff7b38461ea4
Jun Miyakawa <fcfym630@gmail.com>
2018-02-25 15:26:25 +0900

    jsのhtml作成部分をリファクタリング

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     console.log('run buildHTML');
-    var main_html_1 = `<div class='message'>
+    var html =        `<div class='message'>
                         <div class='upper-message'>
                         <div class='upper-message__user-name'>
                           <p>${message.user_name}</p>
@@ -9,28 +9,15 @@ $(function(){
                         <div class='upper-message__date'>
                           <p>${message.created_at}</p>
                         </div>
-                        <div class='lower-meesage'>`;
-    var main_html_2 =    `</div>
+                        <div class='lower-meesage'>
+                        <p class='lower-message__content'>
+                            ${message.content}
+                        </p>
+                        ${ message.image_url == null ? '' : '<img src="' + message.image_url + '">' }
+                        </div>
                           </div>
                          </div>`;
-
-    // lower-meesageの箇所に挿入する部分作成
-    // メッセージの本文があるかどうかで分岐
-    if (message.is_content_present) {
-      var sub_html_1   =  `<p class='lower-message__content'>
-                            ${message.content}
-                           </p>`;
-    } else {
-      var sub_html_1   =  ``;
-    }
-    // 画像の投稿があるかで分岐
-    if (message.is_image_present) {
-      var sub_html_2   =  `<img class='lower-message__image' src='${message.image_url}'`;
-    } else {
-      var sub_html_2   =  ``;
-    }
-
-    return main_html_1 + sub_html_1 + sub_html_2 + main_html_2;
+    return html;
   }
   $('#chat-form').on('submit', function(e){
     e.preventDefault();
